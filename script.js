1bd56ed0602f17c24b7996c296b3ff9aeba0a00f
Collins Opiayo <opiayocollins2@gmail.com>
2025-06-26 22:31:55 +0300

    initial commit

--- script.js
+++ script.js
@@ -42,5 +42,63 @@ const reserveBtn = document.getElementById('reserve-btn');
 
 let cars = [];
 
+//Fetch API
+fetch('http://localhost:3000/cars')
+    .then(res => res.json())
+    .then(data => {
+        cars = data;
+        renderCarList();
+        loadCar(cars[0]);
+    });
 
+//Render Car List
+function renderCarList() {
+    carListEl.innerHTML = '';
+    cars.forEach((car, index) => {
+        const li = document.createElement('li');
+        li.textContent = `${car.Brand} ${car.Model}`;
+        if(index === 0) li.classList.add('active');
 
+        li.addEventListener('click', () => {
+            document.querySelectorAll('.sidebar li').forEach(item => item.classList.remove('active'));
+            li.classList.add('active');
+            loadCar(car);
+        });
+
+        carListEl.appendChild(li);
+    });
+}
+
+//Load car details
+function loadCar(car) {
+    carImageEl.src = car.image || './images/no-image.jpg';
+    priceEl.innerHTML = ` <span>Car Specs</span>`;
+
+    const specs = [
+        ['Brand', car.Brand],
+        ['Model', car.Model],
+        ['Year', car.Year],
+        ['Transmission', car.Transmission],
+        ['Fuel', car.fuel],
+        ['Price', `Ksh ${car.price}`],
+
+    ];
+
+    specsTableEl.innerHTML = specs.map(row =>`
+        <tr>
+        <td>${row[0]}</td><td>${row[1]}</td>
+        </tr>
+        `).join('');
+}
+
+reserveBtn.addEventListener('click', () => {
+    const activeCar = document.querySelector('.sidebar li.active').textContent;
+    alert(`Reservation made for ${activeCar}`);
+});
+
+reserveBtn.addEventListener('mouseover', () => {
+    reserveBtn.style.backgroundColor = '#d3381c';
+});
+reserveBtn.addEventListener('mouseout', () => {
+  reserveBtn.style.backgroundColor = '#ff4d30';
+});
